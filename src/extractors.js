d544a2af2da74e262a5a0aa0413d8c58bc3aba04
Alan Heitkotter <alan@innoplexia.com>
2018-11-23 08:31:27 +0100

    Adjusts to Flow v0.82

--- src/extractors.js
+++ src/extractors.js
@@ -3,26 +3,26 @@
 
 import type { AsyncData } from '../';
 
-export function render<A, R>(ad: AsyncData<A>, f: A => R): ?R {
+export function render<A, R>(ad: AsyncData<A>, f: A => R): R | null {
     return ad.nonEmpty ? f(ad.value) : null;
 }
 
-export function renderReady<A, R>(ad: AsyncData<A>, f: A => R): ?R {
+export function renderReady<A, R>(ad: AsyncData<A>, f: A => R): R | null {
     return ad.isReady ? f(ad.value) : null;
 }
 
-export function renderFailed<A, R>(ad: AsyncData<A>, f: Error => R): ?R {
+export function renderFailed<A, R>(ad: AsyncData<A>, f: Error => R): R | null {
     return ad.isFailed ? f(ad.error) : null;
 }
 
-export function renderPending<A, R>(ad: AsyncData<A>, f: number => R): ?R {
+export function renderPending<A, R>(ad: AsyncData<A>, f: number => R): R | null {
     return ad.isPending ? f(ad.duration().get()) : null;
 }
 
-export function renderStale<A, R>(ad: AsyncData<A>, f: A => R): ?R {
+export function renderStale<A, R>(ad: AsyncData<A>, f: A => R): R | null {
     return ad.isStale ? f(ad.value) : null;
 }
 
-export function renderEmpty<A, R>(ad: AsyncData<A>, f: () => R): ?R {
+export function renderEmpty<A, R>(ad: AsyncData<A>, f: () => R): R | null {
     return ad.isEmpty ? f() : null;
 }
